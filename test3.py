40499658d15f04a4c9a349fd8ad6b5388e41cf00
sudamasahiko <sudamasahiko@gmail.com>
2019-10-11 07:37:15 +0900

    angles are now saved to file

--- test3.py
+++ test3.py
@@ -1,21 +1,28 @@
 # test3.py
+# usage python test3.py [angle]
+# 
+# License: MIT License
 
-import time, sys, math, threading
+import time, sys, math, threading, copy
 import RPi.GPIO as GPIO
 import Adafruit_PCA9685
 
-# constants
+# constants for the motor
 RESOLUTION = 4096
 PULSE_WIDTH_MS = 20.0
 PULSE_FROM = RESOLUTION * 0.5 / PULSE_WIDTH_MS
 PULSE_TO = RESOLUTION * 2.5 / PULSE_WIDTH_MS
+RANGE_ANGLE = 180
 PORT_MOTOR1 = 15
 PORT_MOTOR2 = 14
 PORT_MOTOR3 = 13
+RESOLUTION_MOTOR = 100
+DELAY_STEP = 0.01
+fn_calib = 'calibration.cfg'
+fn_angles = 'angles_last.cfg'
 
 # calibration for the motors
-fn = 'calibration.cfg'
-with open(fn, 'r') as f:
+with open(fn_calib, 'r') as f:
     lines = f.readlines()
     f.close()
 
@@ -26,72 +33,66 @@ calib = []
 for line in lines:
     calib.append(float(line))
 
-GPIO.setmode(GPIO.BOARD)
-pwm = Adafruit_PCA9685.PCA9685()
-pwm.set_pwm_freq(50)
+# saved angles
+with open(fn_angles, 'r') as f:
+    lines = f.readlines()
+    f.close()
 
+if len(lines) != 3:
+    sys.exit()
+
+angles_last = []
+for line in lines:
+    angles_last.append(float(line))
+
+# init
+GPIO.setmode(GPIO.BOARD) # todo xxx should be removed?
+pwm = Adafruit_PCA9685.PCA9685()
+freq = int(1000 / PULSE_WIDTH_MS)
+pwm.set_pwm_freq(freq)
+ports = [PORT_MOTOR1, PORT_MOTOR2, PORT_MOTOR3]
+angles_now = copy.copy(angles_last)
+
+# functions
 def get_pulse(angle):
-    ratio_angle = angle / 180.0
+    ratio_angle = angle / float(RANGE_ANGLE)
     pulse_target = int(PULSE_FROM + ratio_angle * (PULSE_TO - PULSE_FROM))
     return pulse_target
 
-# homing
-pwm.set_pwm(PORT_MOTOR1, 0, get_pulse(calib[0]))
-pwm.set_pwm(PORT_MOTOR2, 0, get_pulse(calib[1]))
-pwm.set_pwm(PORT_MOTOR3, 0, get_pulse(calib[2]))
-ports = [PORT_MOTOR1, PORT_MOTOR2, PORT_MOTOR3]
-time.sleep(1)
-
-deg_m1_last = 0
-deg_m2_last = 0
-deg_m3_last = 0
-
 def set_angle(idx_motor, angle):
-    resolution = 100
-    for i in range(resolution):
-        ratio = math.sin(math.pi * 0.5 * float(i+1) / resolution)
-        computed = calib[idx_motor] - deg_m1_last - ratio * (angle-deg_m1_last)
+    for i in range(RESOLUTION_MOTOR):
+        ratio = math.sin(math.pi * 0.5 * float(i+1) / RESOLUTION_MOTOR)
+        computed = calib[idx_motor] - angles_now[idx_motor] - ratio * (angle-angles_now[idx_motor])
         pwm.set_pwm(ports[idx_motor], 0, get_pulse(computed))
-        time.sleep(0.01)
+        time.sleep(DELAY_STEP)
 
-angle_new = float(sys.argv[1])
-t1 = threading.Thread(target=set_angle, args=(0, angle_new))
-t2 = threading.Thread(target=set_angle, args=(1, angle_new))
-t3 = threading.Thread(target=set_angle, args=(2, angle_new))
-t1.start()
-t2.start()
-t3.start()
-t1.join()
-t2.join()
-t3.join()
-deg_m1_last = angle_new
-deg_m2_last = angle_new
-deg_m3_last = angle_new
-time.sleep(1)
+def drive_motors(angles):
+    global angles_now
+    t1 = threading.Thread(target=set_angle, args=(0, angles[0]))
+    t2 = threading.Thread(target=set_angle, args=(1, angles[1]))
+    t3 = threading.Thread(target=set_angle, args=(2, angles[2]))
+    t1.start()
+    t2.start()
+    t3.start()
+    t1.join()
+    t2.join()
+    t3.join()
+    angles_now[0] = angles[0]
+    angles_now[1] = angles[1]
+    angles_now[2] = angles[2]
+    time.sleep(1)
 
-angle_new = 0
-t1 = threading.Thread(target=set_angle, args=(0, angle_new))
-t2 = threading.Thread(target=set_angle, args=(1, angle_new))
-t3 = threading.Thread(target=set_angle, args=(2, angle_new))
-t1.start()
-t2.start()
-t3.start()
-t1.join()
-t2.join()
-t3.join()
-deg_m1_last = angle_new
-deg_m2_last = angle_new
-deg_m3_last = angle_new
-time.sleep(1)
+# restore the saved angles
+drive_motors(angles_last)
 
-# set_angle(0, 0.0)
-# set_angle(1, 0.0)
-# set_angle(2, 0.0)
-# time.sleep(1)
+# new angles
+ang1 = float(sys.argv[1])
+ang2 = float(sys.argv[2])
+ang3 = float(sys.argv[3])
+drive_motors((ang1, ang2, ang3))
 
-# homing
-# pwm.set_pwm(PORT_MOTOR1, 0, get_pulse(calib[0]))
-# pwm.set_pwm(PORT_MOTOR2, 0, get_pulse(calib[1]))
-# pwm.set_pwm(PORT_MOTOR3, 0, get_pulse(calib[2]))
-# time.sleep(1)
+# todo xxx save present angles to the cfg file
+with open(fn_angles, 'w') as f:
+    for ang in angles_now:
+        f.write('{}\n'.format(ang))
 
